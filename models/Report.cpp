dfe36619fc1e957de1652dbb52cc34009a86edb2
Abdussamet Kocak <sssmmt@gmail.com>
2018-11-12 00:31:50 +0300

    Increase decimal places to 3 digits for reports

--- models/Report.cpp
+++ models/Report.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include "Report.h"
 
-double truncate(double num, int digits = 2) {
+double truncate(double num, int digits = 3) {
     int factor = 10;
     while (--digits) factor *= 10;
     return (double) ((int) (num * factor)) / factor;
@@ -14,7 +14,7 @@ Report::Report(double avgWaitingTime, int missedFlights)
 
 std::ostream& operator<<(std::ostream& os, const Report& report) {
     os << std::fixed;
-    os << std::setprecision(2);
+    os << std::setprecision(3);
     os << truncate(report.avgWaitingTime) << " " << report.missedFlights;
     return os;
 }
